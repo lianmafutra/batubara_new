098df690d0f257cb2975f61c9d98fd0571471cea
Lian Mafutra <lianmafutra@gmail.com>
2023-02-02 21:46:12 +0700

    rename model supir

--- app/Http/Controllers/Kendaraan/SupirController.php
+++ app/Http/Controllers/Kendaraan/SupirController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Kendaraan;
 
 use App\Http\Controllers\Controller;
-use App\Models\supir;
+use App\Models\Supir;
 use App\Utils\ApiResponse;
 use Illuminate\Http\Request;
 
