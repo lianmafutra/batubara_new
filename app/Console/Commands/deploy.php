78279add46cca61991448ad1b47c2a1564a6f054
Lian Mafutra <lianmafutra@gmail.com>
2023-03-04 01:10:32 +0700

    qdwq

--- app/Console/Commands/deploy.php
+++ app/Console/Commands/deploy.php
@@ -84,7 +84,7 @@ class deploy extends Command
          } else {
           
             $progressBar->finish();
-            $this->line('\n<bg=green>git ftp success</>\n');
+            $this->line("\n<bg=green>git ftp success</>\n");
           
             sleep(1.5);
             $this->info("Running : php artisan optimize");
